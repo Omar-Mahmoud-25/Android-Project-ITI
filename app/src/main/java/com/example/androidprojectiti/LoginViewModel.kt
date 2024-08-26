b07369c252ff96ef1bfdfe07488727af40b7e9fe
Omar-Mahmoud-25 <omar.mahmoud25102004@gmail.com>
2024-08-26 12:46:29 +0300

    made the structured of login and signup

---
+++ app/src/main/java/com/example/androidprojectiti/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.androidprojectiti
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // to be continued
+}
--- app/src/main/java/com/example/androidprojectiti/SignUpFragment.kt
+++ app/src/main/java/com/example/androidprojectiti/SignUpFragment.kt
@@ -5,29 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.textfield.TextInputLayout
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SignUpFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SignUpFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var viewModel : SignUpViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,23 +22,29 @@ class SignUpFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_sign_up, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignUpFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SignUpFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = SignUpViewModel()
+        val firstName = view.findViewById<TextInputLayout>(R.id.firstNameTextInput)
+        val lastName = view.findViewById<TextInputLayout>(R.id.lastNameTextInput)
+        val email = view.findViewById<TextInputLayout>(R.id.emailTextInput)
+        val age = view.findViewById<TextInputLayout>(R.id.ageTextInput)
+        val password = view.findViewById<TextInputLayout>(R.id.passwordTextInput)
+        val confirmPassword = view.findViewById<TextInputLayout>(R.id.confirmPasswordTextInput)
+        val signUp = view.findViewById<Button>(R.id.signUpButton)
+        val haveAccount = view.findViewById<TextView>(R.id.haveAccountText)
+
+        haveAccount.setOnClickListener{
+            findNavController().popBackStack()
+        }
+
+        signUp.setOnClickListener{
+            /*
+            * if (viewModel.add(user)
+            *   findNavController().popBackStack()
+            *
+            * */
+        }
+        // to be continued
     }
 }

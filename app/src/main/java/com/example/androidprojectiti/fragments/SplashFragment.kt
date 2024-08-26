9abf8b62a04f4e7912c7c1de1ca9218552100748
Omar-Mahmoud-25 <omar.mahmoud25102004@gmail.com>
2024-08-26 16:27:06 +0300

    validating sign up (not tested yet)

--- app/src/main/java/com/example/androidprojectiti/fragments/SplashFragment.kt
+++ app/src/main/java/com/example/androidprojectiti/fragments/SplashFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.androidprojectiti.R
 
 class SplashFragment : Fragment() {
@@ -17,4 +18,8 @@ class SplashFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_splash, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+    }
 }
--- app/src/main/java/com/example/androidprojectiti/viewModels/SignUpViewModel.kt
+++ app/src/main/java/com/example/androidprojectiti/viewModels/SignUpViewModel.kt
@@ -2,22 +2,77 @@ package com.example.androidprojectiti.viewModels
 
 import androidx.lifecycle.ViewModel
 import com.example.androidprojectiti.database.User
+import com.google.android.material.textfield.TextInputLayout
 
 class SignUpViewModel : ViewModel() {
     private val _emailRegex = Regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$")
     private val _passwordRegex = Regex("^(?=.*[0-9])(?=.*[a-zA-Z]).{8,16}$")
     private fun validateEmail(email: String):Boolean{
-        if (email.matches(_emailRegex)/* and not in the data base */)
-            return true
-        else
+        return email.matches(_emailRegex)
+    }
+
+    private fun validatePassword(password: String):Boolean{
+        return password.matches(_passwordRegex)
+    }
+
+    private fun validateConfirmPassword(confirm:String,password: String):Boolean{
+        return password == confirm
+    }
+
+    fun makeUser(
+        firstName: TextInputLayout,
+        lastName: TextInputLayout,
+        age:TextInputLayout,
+        email:TextInputLayout,
+        password:TextInputLayout,
+        confirmPassword : TextInputLayout
+    ):Boolean{
+        val firstNameText = firstName.editText?.text.toString()
+        val emailText = email.editText?.text.toString()
+        val passwordText = password.editText?.text.toString()
+        val confirmPasswordText = confirmPassword.editText?.text.toString()
+        val ageText = age.editText?.text.toString()
+        val lastNameText = lastName.editText?.text.toString()
+        var valid = true
+        if (firstNameText.isEmpty()) {
+            firstName.error = "This field is required"
+            valid = false
+        }
+        if (lastNameText.isEmpty()){
+            lastName.error = "This field is required"
+            valid = false
+        }
+        if (ageText.isEmpty()){
+            age.error = "This field is required"
+            valid = false
+        }
+        if (!validateEmail(emailText)) {
+            email.error = "Please enter a valid Email!"
+            valid = false
+        }
+        if (!validatePassword(passwordText)) {
+            password.error =
+                "Password must be between 8 and 16 character, containing numbers, upper and lowercase letters"
+            valid = false
+        }
+        if (!validateConfirmPassword(confirmPasswordText,passwordText)) {
+            confirmPassword.error = "Password doesn't match"
+            valid = false
+        }
+        if (!valid)
             return false
-    }
+        // if this email is in the database, return false
 
-    private fun validatePassword():Boolean{
-        return true
-    }
+        val user = User(
+            firstName = firstNameText,
+            lastName = lastNameText,
+            age = ageText.toInt(),
+            email = emailText,
+            password = passwordText
+        )
 
-    fun addUser(user: User):Boolean{
+        // insert user to data base
+
         return true
     }
 

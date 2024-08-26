5a59e63534fc0cdf4ab3cdc92555fb778131cde9
Nada8324 <mohsennada874@gmail.com>
2024-08-26 16:47:34 +0300

    home update

--- app/src/main/java/com/example/androidprojectiti/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/androidprojectiti/fragments/HomeFragment.kt
@@ -1,14 +1,26 @@
 package com.example.androidprojectiti.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.size
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.androidprojectiti.Adapters.CategoryAdapter
 import com.example.androidprojectiti.R
+import com.example.androidprojectiti.category.categoryRepoImp
+import com.example.androidprojectiti.dto.Category
+import com.example.androidprojectiti.network.Api_client
+import com.example.androidprojectiti.viewModels.Home.FactoryClassHome
+import com.example.androidprojectiti.viewModels.Home.HomeViewModel
 
 class HomeFragment : Fragment() {
-
+    lateinit var list_of_Categories: List<Category>
+    private lateinit var retrofitViewModel: HomeViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -16,5 +28,27 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val factoryClass = FactoryClassHome(
+            categoryRepositry = categoryRepoImp(
+                remoteDataSource = Api_client
+            )
+        )
+        retrofitViewModel = ViewModelProvider(this, factoryClass)
+            .get(HomeViewModel::class.java)
+
+
+        retrofitViewModel.getCategories()
+
+        val list = view.findViewById<RecyclerView>(R.id.category_recycler_view)
+        retrofitViewModel.CategoryList.observe(viewLifecycleOwner) {
+            val adapter = CategoryAdapter(it)
+            list_of_Categories = it
+            list.adapter = adapter
+            list.layoutManager = LinearLayoutManager(requireContext())
+Log.d("nada",list.size.toString())
+        }
+    }
 }

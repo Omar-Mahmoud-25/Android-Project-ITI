ebff3c1c265d60354f680856f1d636fd1ff04917
Omar-Mahmoud-25 <omar.mahmoud25102004@gmail.com>
2024-08-30 10:32:52 +0300

    beginning of appbar

--- app/src/main/java/com/example/androidprojectiti/RecipeActivity.kt
+++ app/src/main/java/com/example/androidprojectiti/RecipeActivity.kt
@@ -1,6 +1,12 @@
 package com.example.androidprojectiti
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -9,8 +15,10 @@ import androidx.navigation.ui.NavigationUI
 
 class RecipeActivity : AppCompatActivity() {
     private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var toolbar: Toolbar
     private lateinit var navHostFragment: NavHostFragment
     private lateinit var navController: NavController
+    private lateinit var sharedPreferences : SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,10 +26,17 @@ class RecipeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_food)
 
         bottomNavigationView = findViewById(R.id.bottom_nav)
+        toolbar = findViewById(R.id.toolbar)
         navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         navController = navHostFragment.navController
 
         NavigationUI.setupWithNavController(bottomNavigationView, navController)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Flavor Fusion"
+
+        sharedPreferences = this.
+        getSharedPreferences("logging_details",
+            Context.MODE_PRIVATE)
 
         bottomNavigationView.setOnItemSelectedListener { item ->
             when(item.itemId) {
@@ -39,6 +54,32 @@ class RecipeActivity : AppCompatActivity() {
                 }
                 else -> false
             }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.actions,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.about -> {
+                navController.navigate(R.id.aboutFragment)
+                true
+            }
+            R.id.logOut -> {
+                with(sharedPreferences.edit()) {
+                    putBoolean("isUserLoggedIn", false)
+                    putString("email", "")
+                    apply()
+                }
+                val intent = Intent(this, SplashActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+                true
+            }
+            else -> false
         }
     }
 

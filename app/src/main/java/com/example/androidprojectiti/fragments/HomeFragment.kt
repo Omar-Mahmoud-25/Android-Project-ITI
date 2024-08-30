1044c20982fa8c983a33433f61ce450ca2da8974
oshafcai <oshasaad968@gmail.com>
2024-08-30 18:34:10 +0300

    splash activation

--- app/src/main/java/com/example/androidprojectiti/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/androidprojectiti/fragments/HomeFragment.kt
@@ -23,7 +23,6 @@ import com.example.androidprojectiti.Adapters.MealAdapter
 import com.example.androidprojectiti.R
 import com.example.androidprojectiti.Repositry.category.categoryRepoImp
 import com.example.androidprojectiti.Repositry.meal.mealRepoImp
-import com.example.androidprojectiti.Repositry.user.UserRepo
 import com.example.androidprojectiti.Repositry.user.UserRepoImp
 import com.example.androidprojectiti.database.LocalDataSourceImp
 import com.example.androidprojectiti.database.relations.UserFavorites
@@ -33,14 +32,16 @@ import com.example.androidprojectiti.network.ApiClient
 import com.example.androidprojectiti.network.NetworkLiveData
 import com.example.androidprojectiti.viewModels.Home.FactoryClassHome
 import com.example.androidprojectiti.viewModels.Home.HomeViewModel
+import com.airbnb.lottie.LottieAnimationView
 import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
     lateinit var list_of_Categories: List<Category>
-    lateinit var list_of_meal:List<Meal>
-    lateinit var randomMeal:Meal
+    lateinit var list_of_meal: List<Meal>
+    lateinit var randomMeal: Meal
     private lateinit var retrofitViewModel: HomeViewModel
-    private lateinit var network : NetworkLiveData
+    private lateinit var network: NetworkLiveData
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -48,119 +49,130 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         network = NetworkLiveData(requireContext())
 
-        val sharedPreferences = requireActivity().
-        getSharedPreferences("logging_details",
-            Context.MODE_PRIVATE)
-
-        val email = sharedPreferences.getString("email","guest")
+        val sharedPreferences = requireActivity().getSharedPreferences("logging_details", Context.MODE_PRIVATE)
+        val email = sharedPreferences.getString("email", "guest")
 
         val factoryClass = FactoryClassHome(
-            categoryRepositry = categoryRepoImp(
-                remoteDataSource = ApiClient
-            ),
-            mealRepo=mealRepoImp(
-                remoteDataSource = ApiClient
-            ),
+            categoryRepositry = categoryRepoImp(remoteDataSource = ApiClient),
+            mealRepo = mealRepoImp(remoteDataSource = ApiClient),
             userRepo = UserRepoImp(LocalDataSourceImp(requireContext()))
         )
+
         retrofitViewModel = ViewModelProvider(this, factoryClass)
             .get(HomeViewModel::class.java)
 
-        // Connectivity Manager Code, I can't even know what it does 🤦🏻‍♂️
+        // Find LottieAnimationView
+        val lottieAnimationView: LottieAnimationView = view.findViewById(R.id.lottieAnimationView)
+
+        // Show animation when data is loading
+        fun showLoading() {
+            lottieAnimationView.visibility = View.VISIBLE
+            lottieAnimationView.playAnimation()
+        }
+
+        // Hide animation when data is loaded or error occurs
+        fun hideLoading() {
+            lottieAnimationView.visibility = View.GONE
+            lottieAnimationView.cancelAnimation()
+        }
+
         network.observe(requireActivity()) {
             if (it) {
+                showLoading()
                 retrofitViewModel.getMeals()
                 retrofitViewModel.getCategories()
-            }
-            else
+            } else {
                 Toast.makeText(requireContext(), "No Internet", Toast.LENGTH_LONG).show()
+            }
         }
 
         val Categorieslist = view.findViewById<RecyclerView>(R.id.category_recycler_view)
         retrofitViewModel.CategoryList.observe(viewLifecycleOwner) {
+            hideLoading()
             val adapter = CategoryAdapter(it)
             list_of_Categories = it
             Categorieslist.adapter = adapter
-            Categorieslist.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
+            Categorieslist.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
         }
+
         val Mealslist = view.findViewById<RecyclerView>(R.id.meal_recycler_view)
         retrofitViewModel.MealsList.observe(viewLifecycleOwner) {
-            // passing a user repo for favorite
+            hideLoading()
             val adapter = MealAdapter(
                 it,
                 UserRepoImp(LocalDataSourceImp(requireContext())),
                 lifecycleScope = lifecycleScope,
-                email = email?: "guest",
-
+                email = email ?: "guest"
             )
             list_of_meal = it
             Mealslist.adapter = adapter
-            Mealslist.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
+            Mealslist.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
         }
-        var name=view.findViewById<TextView>(R.id.Name)
-        val nameOfUser = view.findViewById<TextView>(R.id.nameOfUser)
-        var category=view.findViewById<TextView>(R.id.CategoryName)
-        var image=view.findViewById<ImageView>(R.id.imageView)
-        var favouriteButton = view.findViewById<ImageButton>(R.id.heart_button)
 
-        retrofitViewModel.getUserName(email?:"guest")
-        retrofitViewModel.userName.observe(viewLifecycleOwner){
+        val name = view.findViewById<TextView>(R.id.Name)
+        val nameOfUser = view.findViewById<TextView>(R.id.nameOfUser)
+        val category = view.findViewById<TextView>(R.id.CategoryName)
+        val image = view.findViewById<ImageView>(R.id.imageView)
+        val favouriteButton = view.findViewById<ImageButton>(R.id.heart_button)
+
+        retrofitViewModel.getUserName(email ?: "guest")
+        retrofitViewModel.userName.observe(viewLifecycleOwner) {
             nameOfUser.text = it
         }
+
         retrofitViewModel.getRandomMeal()
-        retrofitViewModel.RandomMeal.observe(viewLifecycleOwner){
-            randomMeal=it[0]
-            name.text=randomMeal.strMeal
-            category.text=randomMeal.strCategory
+        retrofitViewModel.RandomMeal.observe(viewLifecycleOwner) {
+            randomMeal = it[0]
+            name.text = randomMeal.strMeal
+            category.text = randomMeal.strCategory
             Glide.with(image.context)
                 .load(randomMeal.strMealThumb)
                 .placeholder(R.drawable.baseline_arrow_circle_down_24)
                 .error(R.drawable.baseline_error_24)
                 .into(image)
+
             lifecycleScope.launch {
                 val userRepo = UserRepoImp(LocalDataSourceImp(requireContext()))
                 val favoriteMeals = userRepo.getUserFavoriteMeals(email ?: "guest")
                 var isFavorite = favoriteMeals.contains(randomMeal.idMeal)
 
                 favouriteButton.setImageResource(
-                    if (isFavorite)
-                    {
+                    if (isFavorite) {
                         R.drawable.red_heart
-                    }
-                    else {
+                    } else {
                         R.drawable.white_heart
                     }
                 )
+
                 favouriteButton.setOnClickListener {
                     if (isFavorite) {
                         favouriteButton.setImageResource(R.drawable.white_heart)
                         lifecycleScope.launch {
-                            userRepo.deleteMealFromFav(
-                                UserFavorites(email ?: "guest", randomMeal.idMeal)
-                            )
+                            userRepo.deleteMealFromFav(UserFavorites(email ?: "guest", randomMeal.idMeal))
                             Toast.makeText(
                                 requireContext(),
-                                "${randomMeal.strMeal} removed from favorites", Toast.LENGTH_SHORT).show()
+                                "${randomMeal.strMeal} removed from favorites", Toast.LENGTH_SHORT
+                            ).show()
                         }
                     } else {
                         favouriteButton.setImageResource(R.drawable.red_heart)
                         lifecycleScope.launch {
-                            userRepo.insertMealToFav(
-                                UserFavorites(email ?: "guest", randomMeal.idMeal)
-                            )
-                            Toast.makeText(requireContext(), "${randomMeal.strMeal} added to favorites", Toast.LENGTH_SHORT).show()
+                            userRepo.insertMealToFav(UserFavorites(email ?: "guest", randomMeal.idMeal))
+                            Toast.makeText(
+                                requireContext(),
+                                "${randomMeal.strMeal} added to favorites", Toast.LENGTH_SHORT
+                            ).show()
                         }
                     }
-                    isFavorite=!isFavorite
+                    isFavorite = !isFavorite
                 }
             }
         }
     }
-
-
 }

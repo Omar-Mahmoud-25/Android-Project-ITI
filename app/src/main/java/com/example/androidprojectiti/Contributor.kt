dfbace256874b9d9f1fbbbcc0320582e345c48e4
Sharawelly <lordmarawanshrief@gmail.com>
2024-09-03 22:14:33 +0300

    handle ui

---
+++ app/src/main/java/com/example/androidprojectiti/Contributor.kt
@@ -0,0 +1,3 @@
+package com.example.androidprojectiti
+
+data class Contributor(var image : Int, var name: String, var email : String)
--- app/src/main/java/com/example/androidprojectiti/fragments/AboutFragment.kt
+++ app/src/main/java/com/example/androidprojectiti/fragments/AboutFragment.kt
@@ -5,9 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.androidprojectiti.Adapters.AboutUsAdapter
+import com.example.androidprojectiti.Contributor
 import com.example.androidprojectiti.R
 
 class AboutFragment : Fragment() {
+
+    private val contributors = listOf(
+        Contributor(R.drawable.white_heart,"Marawan Shrief Abdelatef", "lordmarawanshrief@gmail.com"),
+        Contributor(R.drawable.circle_green_background,"Nadra Mahmoud Saad", "oshasaad968@gmail.com"),
+        Contributor(R.drawable.red_heart, "Nada Mohsen Ahmed", "mohsennada874@gmail.com"),
+        Contributor(R.drawable.circle_green_background,"Omar Mahmoud Abdullah", "omar.mahmoud25102004@gmail.com")
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +31,10 @@ class AboutFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         activity?.title = "About"
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.about_us_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = AboutUsAdapter(contributors)
     }
 
 }
